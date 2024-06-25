ff11e102bccd318d19fe0e11d46e0cc08b7f8d37
Thomas <thomas.doppler00@gmail.com>
2024-06-25 11:03:55 +0200

    fix champs bdd

--- Script_SQL/initBDD.sql
+++ Script_SQL/initBDD.sql
@@ -6,6 +6,7 @@ CREATE TABLE Categorie (
 CREATE TABLE Sous_categorie (
   Id INT PRIMARY KEY AUTO_INCREMENT,
   Nom VARCHAR(255) NOT NULL,
+  Description TEXT,
   Categorield INT,
   FOREIGN KEY (Categorield) REFERENCES Categorie(Id),
   INDEX idx_Sous_categorie_Categorield (Categorield)
@@ -13,8 +14,10 @@ CREATE TABLE Sous_categorie (
 
 CREATE TABLE Question (
   Id INT PRIMARY KEY AUTO_INCREMENT,
-  Intitulé VARCHAR(255) NOT NULL,
-  Commentaires TEXT,
+  Intitule VARCHAR(255) NOT NULL,
+  Reponse0 VARCHAR(255),
+  Reponse1 VARCHAR(255),
+  Reponse2 VARCHAR(255),
   SousCategorield INT,
   FOREIGN KEY (SousCategorield) REFERENCES Sous_categorie(Id),
   INDEX idx_Question_SousCategorield (SousCategorield)
